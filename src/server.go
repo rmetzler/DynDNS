0e326700b64a4fe9f3776d4d6ec9dd7ebe1fd2df
Richard Metzler <r.metzler@cpu-24-7.com>
2018-08-07 16:02:28 +0200

    go fmt .

--- src/server.go
+++ src/server.go
@@ -7,47 +7,46 @@ package main
  *
  * Done by Bastian Bringenberg <bastian@agentur-pottkinder.de>
  * External IP from: <http://myexternalip.com/#golang>
-*/
+ */
 
 import (
-	"net/http"
-	"log"
-	"os"
-	"net/url"
-	"time"
-	"io/ioutil"
 	"github.com/BurntSushi/toml"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"net/url"
+	"os"
+	"time"
 )
 
 type Config struct {
-    Cnamemaster string
-    Subdomain   string
-    Domain      string
-    User        string
-    Pass        string
+	Cnamemaster string
+	Subdomain   string
+	Domain      string
+	User        string
+	Pass        string
 }
 
-
-func getIp(requestType string) (string) {
-	res, _ := http.Get("http://" + requestType  + ".myexternalip.com/raw")
+func getIp(requestType string) string {
+	res, _ := http.Get("http://" + requestType + ".myexternalip.com/raw")
 	defer res.Body.Close()
 
-        ip, _ := ioutil.ReadAll(res.Body)
+	ip, _ := ioutil.ReadAll(res.Body)
 	return string(ip[:])
 }
 
 func readConfigfile() Config {
-    var configfile = "properties.ini"
-    _, err := os.Stat(configfile)
-    if err != nil {
-        log.Fatal("Config file is missing: ", configfile)
-    }
+	var configfile = "properties.ini"
+	_, err := os.Stat(configfile)
+	if err != nil {
+		log.Fatal("Config file is missing: ", configfile)
+	}
 
-    var config Config
-    if _, err := toml.DecodeFile(configfile, &config); err != nil {
-        log.Fatal(err)
-    }
-    return config
+	var config Config
+	if _, err := toml.DecodeFile(configfile, &config); err != nil {
+		log.Fatal(err)
+	}
+	return config
 }
 
 func dyndns(ipv4 string, ipv6 string, config Config) {
@@ -57,12 +56,12 @@ func dyndns(ipv4 string, ipv6 string, config Config) {
 	v.Add("s_login", config.User)
 	v.Add("s_pw", config.Pass)
 	v.Add("command", "UpdateDNSZone")
-	v.Add("dnszone", config.Domain + ".")
-	v.Add("rr0", config.Domain + ". 3600 IN CNAME " + config.Cnamemaster + ".")
-	v.Add("rr1", "*." + config.Domain + ". 3600 IN CNAME " + config.Cnamemaster + ".")
-	v.Add("rr2", config.Subdomain + ". 600 IN A " + ipv4)
-	v.Add("rr3", config.Subdomain + ". 600 IN AAAA " + ipv6)
-	v.Add("rr4", config.Subdomain + ". 600 IN TXT " + formattedTime)
+	v.Add("dnszone", config.Domain+".")
+	v.Add("rr0", config.Domain+". 3600 IN CNAME "+config.Cnamemaster+".")
+	v.Add("rr1", "*."+config.Domain+". 3600 IN CNAME "+config.Cnamemaster+".")
+	v.Add("rr2", config.Subdomain+". 600 IN A "+ipv4)
+	v.Add("rr3", config.Subdomain+". 600 IN AAAA "+ipv6)
+	v.Add("rr4", config.Subdomain+". 600 IN TXT "+formattedTime)
 	resp, err := http.PostForm("https://api.domainreselling.de/api/call.cgi", v)
 	if err != nil {
 		log.Fatal(err)
@@ -77,7 +76,7 @@ func main() {
 	for {
 		ipv4 := getIp("ipv4")
 		ipv6 := getIp("ipv6")
-		if(lastIpv4 != ipv4 || lastIpv6 != ipv6) {
+		if lastIpv4 != ipv4 || lastIpv6 != ipv6 {
 			log.Print("Ipv4 or Ipv6 altered")
 			log.Print("IPv4: " + ipv4)
 			log.Print("IPv6: " + ipv6)
